3b0a5d451fb59e7298832a99bf073882e1d5e0c6
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 07:02:29 +1000

    Added schedule page

---
+++ page-schedule.php
@@ -0,0 +1,118 @@
+<?php
+/* Template Name: Schedule Page */
+get_header(); ?>
+
+<div class="schedule-container">
+	<h1 class="schedule-heading">Current Sessions for 2025</h1>
+	
+	<div class="schedule-table-wrapper">
+		<table class="schedule-table">
+			<tr>
+				<th>Subjects</th>
+				<th>Weekdays</th>
+				<th>Weekends</th>
+			</tr>
+			<tr>
+				<td>Primary 3-6 Science<br><strong>Monthly $100</strong></td>
+				<td>
+					Mon, 4pm - 6pm, 7pm - 9pm<br>
+					Wed, 4pm - 6pm, 7pm - 9pm<br>
+					Fri, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 8am – 10am, 1pm - 3pm<br>
+					Sun, 8am – 10am, 1pm - 3pm
+				</td>
+			</tr>
+			<tr>
+				<td>Primary 3-6 Math<br><strong>Monthly $100</strong></td>
+				<td>
+					Mon, 4pm - 6pm, 7pm - 9pm<br>
+					Wed, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 8am – 10am, 1pm - 3pm<br>
+					Sun, 8am – 10am, 1pm - 3pm
+				</td>
+			</tr>
+			<tr>
+				<td>Secondary 1-2 Science<br><strong>Monthly $150</strong></td>
+				<td>
+					Tue, 4pm - 6pm, 7pm - 9pm<br>
+					Thu, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 10am – 12pm, 4pm – 6pm<br>
+					Sun, 10am – 12pm, 4pm – 6pm
+				</td>
+			</tr>
+			<tr>
+				<td>Secondary 1-2 Math<br><strong>Monthly $150</strong></td>
+				<td>
+					Tue, 4pm - 6pm, 7pm - 9pm<br>
+					Thu, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 10am – 12pm, 4pm – 6pm<br>
+					Sun, 10am – 12pm, 4pm – 6pm
+				</td>
+			</tr>
+			<tr>
+				<td>Secondary 3-4 Science<br><strong>Monthly $200</strong></td>
+				<td>
+					Tue, 4pm - 6pm, 7pm - 9pm<br>
+					Thu, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 10am – 12pm, 4pm – 6pm<br>
+					Sun, 10am – 12pm, 4pm – 6pm
+				</td>
+			</tr>
+			<tr>
+				<td>Secondary 3-4 Math<br><strong>Monthly $200</strong></td>
+				<td>
+					Tue, 4pm - 6pm, 7pm - 9pm<br>
+					Thu, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 10am – 12pm, 4pm – 6pm<br>
+					Sun, 10am – 12pm, 4pm – 6pm
+				</td>
+			</tr>
+			<tr>
+				<td>JC 1-2 Science<br><strong>Monthly $250</strong></td>
+				<td>
+					Mon, 4pm - 6pm, 7pm - 9pm<br>
+					Wed, 4pm - 6pm, 7pm - 9pm<br>
+					Fri, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 8am – 10am, 1pm - 3pm<br>
+					Sun, 8am – 10am, 1pm - 3pm
+				</td>
+			</tr>
+			<tr>
+				<td>JC 1-2 Math<br><strong>Monthly $250</strong></td>
+				<td>
+					Mon, 4pm - 6pm, 7pm - 9pm<br>
+					Wed, 4pm - 6pm, 7pm - 9pm<br>
+					Fri, 4pm - 6pm, 7pm - 9pm
+				</td>
+				<td>
+					Sat, 8am – 10am, 1pm - 3pm<br>
+					Sun, 8am – 10am, 1pm - 3pm
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<div class="schedule-promotions">
+		<h2>Promotions <small>(Registration before <strong>1<sup>st</sup> May 2025</strong>)</small></h2>
+		<ul>
+			<li>For students that enrolled into 2 subjects, there will be a 5% discount on the monthly fees.</li>
+			<li>For students that enrolled into 3 subjects, there will be a 10% discount on the monthly fees.</li>
+		</ul>
+	</div>
+</div>
+
+<?php get_footer(); ?>
