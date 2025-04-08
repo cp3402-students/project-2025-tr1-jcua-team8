8c47d96a4c471b85eecb2a6cc6dd8d4826d7c9e6
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 08:33:35 +1000

    Added testimonials page

---
+++ page-testimonials.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: Testimonials Page */
+get_header(); ?>
+
+<div class="testimonials-container">
+  <h1 class="testimonials-heading">What Parents and Students Say</h1>
+
+  <div class="testimonial">
+    <p class="quote">“Baizonn Learning Centre has provided more exposure for Glen. I am grateful to the teachers as they displayed patience to understand my son’s learning style and followed through in his learning journey.”</p>
+    <p class="author">- Mdm Sharon Koo, Glen’s Mother (Secondary 3)</p>
+  </div>
+<?php get_footer(); ?>
