cd56ee0a664542d7aaa10056aba697c80d20932d
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 07:48:20 +1000

    Added faculty page

---
+++ page-faculty.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: Teachers Page */
+get_header(); ?>
+
+<div class="teachers-container">
+  <h1 class="teachers-heading">Meet Our Teachers</h1>
+
+  <div class="teacher-profile">
+    <img src="<?php echo get_template_directory_uri(); ?>/Assets/images/Teacher Ms Siti.png" alt="Teacher Ms Siti">
+    <div class="teacher-info">
+      <h2>Ms Siti</h2>
+      <p><strong>Qualification:</strong> Bachelor in Science</p>
+      <p><strong>Experience:</strong> 8 Years</p>
+      <p>Ms Siti is well equipped in engaging children with knowledge and fun that comes hand in hand in Smart Science Lab. Her 8-year experience helps lead students in striving to be better in both academics and well-being. Her enthusiasm encourages students to not only do better but also express themselves more clearly and understand their values.</p>
+    </div>
+  </div>
+  <?php get_footer(); ?>
