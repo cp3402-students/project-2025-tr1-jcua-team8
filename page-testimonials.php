5b75428d27ee6926e0870a9a82faa867c3d6deb9
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 09:01:45 +1000

    Updated testimonials

--- page-testimonials.php
+++ page-testimonials.php
@@ -9,4 +9,30 @@ get_header(); ?>
     <p class="quote">“Baizonn Learning Centre has provided more exposure for Glen. I am grateful to the teachers as they displayed patience to understand my son’s learning style and followed through in his learning journey.”</p>
     <p class="author">- Mdm Sharon Koo, Glen’s Mother (Secondary 3)</p>
   </div>
+
+  <div class="testimonial">
+    <p class="quote">“Natalie has learnt a lot of things not covered by the usual textbooks or school materials. Teachers have been very encouraging and supportive to Natalie.”</p>
+    <p class="author">- Mr Ben Tan, Natalie’s Father (Primary 5)</p>
+  </div>
+
+  <div class="testimonial">
+    <p class="quote">“Baizonn Learning Centre promotes curiosity in Saifu and boosts his confidence in Math. Teacher Ms Yati is very good at keeping her curious.”</p>
+    <p class="author">- Mdm Yani, Saifu’s Mother (Secondary 2)</p>
+  </div>
+
+  <div class="testimonial">
+    <p class="quote">“Baizonn Learning Centre has expanded my learning circle. The notes provided are very comprehensive and weekly hands-on activities kept me interested in Science.”</p>
+    <p class="author">- Owen Tan ZhiHao (Primary 6)</p>
+  </div>
+
+  <div class="testimonial">
+    <p class="quote">“Baizonn Learning Centre programs have helped me achieve AL1 in all subjects for PSLE. Teachers here are very engaging, motivating and interactive.”</p>
+    <p class="author">- Sophie Huang XiaoLi (Primary 6)</p>
+  </div>
+
+  <div class="testimonial">
+    <p class="quote">“The program in Baizonn Learning Centre has built up his confidence and helped him excel in his PSLE. Baizonn has a team of dedicated tutors — a commendable effort.”</p>
+    <p class="author">- Mdm Melanie Tan, Ethan Phua’s Mother</p>
+  </div>
+</div>
 <?php get_footer(); ?>
