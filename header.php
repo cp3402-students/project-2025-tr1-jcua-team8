680bb4d1cf69081ee5d53583beca32efdbff4c9c
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 19:15:33 +1000

    Updater logo to be clickable

--- header.php
+++ header.php
@@ -23,9 +23,9 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-top">
-			<img src="<?php echo get_template_directory_uri(); ?>/Assets/images/Centre Logo 1.png" alt="Baizonn Learning Centre Logo" class="site-logo">
-
-			
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="custom-logo-link">
+				<img src="<?php echo get_template_directory_uri(); ?>/Assets/images/Centre Logo 1.png" alt="Baizonn Learning Centre Logo" class="site-logo">
+			</a>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div>
 
@@ -42,4 +42,6 @@
 	</header>
 
 
-	<div id="content" class="site-content">
+
+
+<div id="content" class="site-content">
