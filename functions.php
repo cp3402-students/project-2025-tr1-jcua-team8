f52a23734f620450d1184a625f8d279ece16c795
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 21:49:48 +1000

    Done correction

--- functions.php
+++ functions.php
@@ -99,6 +99,13 @@ function baizonn_setup() {
 			'flex-height' => true,
 		)
 	);
+	// Recommended modern theme support features
+	add_theme_support( 'wp-block-styles' );
+	add_theme_support( 'responsive-embeds' );
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'style.css' );
+	
 }
 add_action( 'after_setup_theme', 'baizonn_setup' );
 
