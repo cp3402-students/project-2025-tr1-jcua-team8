a401cc7266613981583fe1e71c0bb59524830866
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-09 12:05:26 +1000

    Added page open house

---
+++ page-open-house.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Open House
+ */
+get_header(); ?>
+
+<div class="open-house-container">
+    <h1 class="highlight-red">Open House for 2025 Intake</h1>
+    <p class="highlight-blue"><strong>12<sup>th</sup> April 2025, Saturday, 10am to 6pm</strong></p>
+
+    <p>Bookings are essential. Kindly make your booking as soon as possible so that you will not be disappointed.</p>
+    
+    <p>Please contact us on the Center’s email at 
+        <a href="mailto:BaizonnLearning@gmail.com">BaizonnLearning@gmail.com</a> 
+        or via phone <strong>+65 62811816</strong>.
+    </p>
+
+    <p>Buffet lunch will be arranged for all guests. All food is Halal and vegetarian menu is available too. 
+    Please indicate your preference during booking.</p>
+
+    <p><strong>🎁 Unique Baizonn T-Shirt</strong> will be given free for all confirmed students during the Open House!</p>
+
+    <div class="tshirt-image">
+        <img src="<?php echo get_template_directory_uri(); ?>/Baizonn Content/T-Shirt.png" alt="Baizonn T-Shirt">
+    </div>
+</div>
+
+<?php get_footer(); ?>
