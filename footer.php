ca4c998b39bcdc05eeb9ab66aba9545e203fd004
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-04-10 20:19:02 +1000

    Update quick links

--- footer.php
+++ footer.php
@@ -21,10 +21,15 @@
     <div class="footer-column">
       <h3>Quick Links</h3>
       <p><a href="<?php echo esc_url( home_url()); ?>">Home</a></p>
-      <p><a href="<?php echo esc_url( home_url('/about')); ?>">About</a></p>
+      <p><a href="<?php echo esc_url( home_url('/about')); ?>">About Us</a></p>
       <p><a href="<?php echo esc_url( home_url('/programs')); ?>">Programs</a></p>
-      <p><a href="<?php echo esc_url( home_url('/contact')); ?>">Contact</a></p>
-      <p><a href="<?php echo esc_url( home_url('/open-house')); ?>">Open House</a></p>
+      <p><a href="<?php echo esc_url( home_url('/faculty')); ?>">Faculty</a></p>
+      <p><a href="<?php echo esc_url( home_url('/testimonials')); ?>">Testimonials</a></p>
+      <p><a href="<?php echo esc_url( home_url('/schedule')); ?>">Fee & Schedule</a></p>
+      <p><a href="<?php echo esc_url( home_url('/register')); ?>">Register</a></p>
+      <p><a href="<?php echo esc_url( home_url('/open-house')); ?>">Announcements</a></p>
+      <p><a href="<?php echo esc_url( home_url('/contact')); ?>">Contact Us</a></p>
+      
     </div>
     <div class="footer-column">
       <h3>Follow Us</h3>
